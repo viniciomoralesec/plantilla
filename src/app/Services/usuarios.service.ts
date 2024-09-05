401475ac6508887b4ebc33b949514d1f9c9e0787
viniciomoralesec <viniciomorales.uio@gmail.com>
2024-09-05 15:35:59 -0500

    update

--- src/app/Services/usuarios.service.ts
+++ src/app/Services/usuarios.service.ts
@@ -13,7 +13,9 @@ export class UsuariosService {
   constructor(
     private lector: HttpClient,
     private navegacion: Router
-  ) {}
+  ) {
+    this.checkLoginStatus(); // Verificar el estado de inicio de sesión al iniciar el servicio
+  }
   login(usuario: IUsuarios) {
     let formData = new FormData();
     formData.append('Nombre_Usuario', usuario.Nombre_Usuario);
